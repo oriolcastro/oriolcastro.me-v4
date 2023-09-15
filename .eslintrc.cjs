e949bbe8d1671d50926138ee89d9b01a66447a22
Oriol <oriol.castroarnau@gmail.com>
2023-09-15 16:03:25 +0200

    chore: update general configs
    - Add import sorting for ESLint
    - Update Prettier preferences
    - Add commitlint
    - Add lint staged and git hooks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,13 +6,15 @@ module.exports = {
 		node: true,
 	},
 	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint", "prettier"],
+	plugins: ["@typescript-eslint", "simple-import-sort", "prettier"],
 	extends: [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
 		"plugin:prettier/recommended",
 		"plugin:astro/recommended",
 		"plugin:astro/jsx-a11y-recommended",
+		"plugin:import/recommended",
+		"plugin:import/typescript",
 	],
 	rules: {
 		"@typescript-eslint/no-var-requires": "warn",
@@ -20,6 +22,14 @@ module.exports = {
 			"warn",
 			{ varsIgnorePattern: "Props", ignoreRestSiblings: true },
 		],
+		"simple-import-sort/imports": "error",
+		"simple-import-sort/exports": "error",
+		"import/first": "error",
+		"import/newline-after-import": "error",
+		"import/no-duplicates": "error",
+		"import/prefer-default-export": "off",
+		"import/no-cycle": "warn",
+		"import/no-unresolved": "off",
 	},
 	overrides: [
 		{
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,17 +1,18 @@
 /** @type {import("@types/prettier").Options} */
 module.exports = {
-	printWidth: 100,
-	semi: true,
-	singleQuote: false,
-	tabWidth: 2,
-	useTabs: true,
-	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss" /* Must come last */],
-	overrides: [
-		{
-			files: "*.astro",
-			options: {
-				parser: "astro",
-			},
-		},
-	],
-};
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss' /* Must come last */],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
+{
+}
